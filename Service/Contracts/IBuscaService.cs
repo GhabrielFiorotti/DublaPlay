29eb81dcbd8e3135064c86dbdc95e6e4167b4a63
Rusley Santos <rusley.santos@uniodigital.com.br>
2020-11-23 20:44:51 -0300

    Adiciona consultas

--- Service/Contracts/IBuscaService.cs
+++ Service/Contracts/IBuscaService.cs
@@ -12,5 +12,10 @@ namespace Service.Contracts
         public Orcamento BuscarOrcamento(Orcamento orcamento);
         public Solicitacao BuscarSolicitacao(Solicitacao solicitacao);
         public Usuario BuscarUsuario(Usuario usuario);
+        List<Contrato> BuscarContratos(int page, int size);
+        List<Empresa> BuscarEmpresas(int page, int size);
+        List<Orcamento> BuscarOrcamentos(int page, int size);
+        List<Solicitacao> BuscarSolicitacoes(int page, int size);
+        List<Usuario> BuscarUsuarios(int page, int size);
     }
 }
