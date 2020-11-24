284633560c4dbe6b70a5e7ed53bad9949f67191e
Rusley Santos <rusley.santos@uniodigital.com.br>
2020-11-23 21:30:48 -0300

    Adiciona operator

---
+++ Common/Common/GravarArquivo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Common
+{
+    public class GravarArquivo
+    {
+        public abstract void Gravar(string dados);
+    }
+}
