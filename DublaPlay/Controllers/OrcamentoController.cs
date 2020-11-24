cac47f9dfcb6c155ae4e5160ef15db3c0e5cc77b
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-11-23 21:24:53 -0300

    Modificações do controller

--- DublaPlay/Controllers/OrcamentoController.cs
+++ DublaPlay/Controllers/OrcamentoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Repository.Models;
 using Repository.ViewModel;
 using Service.Contracts;
 
@@ -20,7 +21,33 @@ namespace DublaPlay.Controllers
         [HttpPost("[action]")]
         public IActionResult CadastroOrcamento([FromBody] OrcamentoViewModel orcamento)
         {
-            return Ok(_service.CadastrarOrcamento(orcamento));
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+
+                    return Ok(new MessageReturn("Sucesso ao Adicionar Projeto",
+                                                "",
+                                                true,
+                                                  _service.CadastrarOrcamento(orcamento)));
+
+                }
+                else
+                {
+                    return BadRequest(new MessageReturn("Erro ao Adicionar Projeto",
+                                                        "Preencha todos os campos.",
+                                                        false));
+                }
+            }
+            catch
+            {
+                return BadRequest(new MessageReturn("Erro ao Adicionar Projeto",
+                                                   "Erro ao adicionar projeto, por favor tente novamente mais tarde.",
+                                                   false));
+
+            }
         }
     }
 }
