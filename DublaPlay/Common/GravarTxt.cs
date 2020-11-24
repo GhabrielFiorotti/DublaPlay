c6fd5a2b4c9589dd9d6e913f990cf6cce6c4f657
Rusley Santos <rusley.santos@uniodigital.com.br>
2020-11-23 22:51:25 -0300

    Adiciona sobrecarga

---
+++ DublaPlay/Common/GravarTxt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DublaPlay.Common
+{
+    public class GravarTxt : GravarArquivo
+    {
+        public string Dados { get; set; }
+        public override void Gravar(string dados)
+        {
+            string fileName = @"C:\Temp\log.txt";
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                using (FileStream fs = File.Create(fileName))
+                {
+                    Byte[] title = new UTF8Encoding(true).GetBytes(dados);
+                    fs.Write(title, 0, title.Length);
+                    byte[] author = new UTF8Encoding(true).GetBytes("Login");
+                    fs.Write(author, 0, author.Length);
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+        }
+
+        public static GravarTxt operator /(GravarTxt gravar, char sepatador)
+        {
+            GravarTxt gravarSplit = new GravarTxt();
+            gravarSplit.Dados = gravar.Dados.Split(sepatador).ToString();
+
+            return gravarSplit;
+        }
+    }
+}
+
+
